f81f71bf6a02508e3ac3f5828591d182958c20c4
Blagojevic-Bogdan <bb20190479@student.fon.bg.ac.rs>
2024-09-12 00:42:28 +0200

    Dokumentovanje klase Odgovor

--- Zajednicki/src/main/java/komunikacija/Odgovor.java
+++ Zajednicki/src/main/java/komunikacija/Odgovor.java
@@ -8,23 +8,52 @@ import java.io.Serializable;
 import java.util.Objects;
 
 /**
- *
+ * Klasa koja predstavlja odgovor koji se koristi za komunikaciju između klijenta i servera.
+ * <p>
+ * Ova klasa implementira interfejs {@link Serializable} što omogućava serijalizaciju objekta
+ * za prenos podataka između klijenta i servera.
+ * </p>
+ * 
  * @author Bogdan Blagojevic
  */
 public class Odgovor implements Serializable{
+        /** 
+     * Objekat koji predstavlja odgovor servera.
+     * Može biti bilo koje vrste podataka koji je potrebno preneti klijentu.
+     */
     private Object odgovor;
 
+    
+        /**
+     * Podrazumevani konstruktor koji inicijalizuje objekat {@code Odgovor}.
+     * Ovaj konstruktor ne postavlja nikakve vrednosti za {@code odgovor}.
+     */
     public Odgovor() {
     }
 
+        /**
+     * Parametarski konstruktor koji inicijalizuje objekat {@code Odgovor} sa datim odgovorom.
+     *
+     * @param odgovor Objekat koji predstavlja odgovor servera.
+     */
     public Odgovor(Object odgovor) {
         this.odgovor = odgovor;
     }
 
+        /**
+     * Get metoda - Vraća odgovor servera.
+     *
+     * @return Objekat koji predstavlja odgovor servera.
+     */
     public Object getOdgovor() {
         return odgovor;
     }
 
+        /**
+     * Set metoda - Postavlja odgovor servera.
+     *
+     * @param odgovor Objekat koji predstavlja odgovor servera.
+     */
     public void setOdgovor(Object odgovor) {
         this.odgovor = odgovor;
     }
@@ -35,6 +64,13 @@ public class Odgovor implements Serializable{
         return hash;
     }
 
+        /**
+     * Upoređuje objekat {@code Odgovor} sa drugim objekatom.
+     *
+     * @param obj Drugi objekat koji se upoređuje.
+     * @return {@code true} ako su objekti identični, {@code false} inače.
+     * Poređenje se vrši na osnovu vrednosti {@code odgovor}.
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
