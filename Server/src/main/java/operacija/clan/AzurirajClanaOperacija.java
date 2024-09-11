346f23773e2ed2fc381531602ed77de780b345f0
Blagojevic-Bogdan <bb20190479@student.fon.bg.ac.rs>
2024-09-12 01:26:42 +0200

    Dokumentovan paket operacija.clan

--- Server/src/main/java/operacija/clan/AzurirajClanaOperacija.java
+++ Server/src/main/java/operacija/clan/AzurirajClanaOperacija.java
@@ -8,17 +8,53 @@ import domen.Clan;
 import operacija.ApstraktnaGenerickaOperacija;
 
 /**
- *
+ * Klasa koja predstavlja operaciju ažuriranja podataka o članu u bazi podataka.
+ * <p>
+ * Nasleđuje klasu {@link ApstraktnaGenerickaOperacija} i implementira konkretne metode
+ * za ažuriranje podataka o članu. Ova klasa koristi šablon za obavljanje operacije
+ * i upravljanje transakcijama u bazi podataka.
+ * </p>
+ * 
  * @author Bogdan Blagojevic
  */
 public class AzurirajClanaOperacija extends ApstraktnaGenerickaOperacija{
+        /**
+     * Staticka promenljiva koja označava rezultat ažuriranja.
+     * <p>
+     * Ova promenljiva je postavljena na {@code false} kada se operacija započne i ažurira se
+     * tokom izvršenja operacije. Pokušava se da se ažurira podatak o članu i rezultat se čuva
+     * u ovoj promenljivoj.
+     * </p>
+     */
     public static boolean update = false;
+    
+        /**
+     * Proverava preduslove za izvršenje operacije.
+     * <p>
+     * Ova metoda osigurava da parametar koji se prosleđuje bude instanca {@link Clan}.
+     * Ako parametar nije instanca {@link Clan} ili je {@code null}, baca se izuzetak.
+     * </p>
+     * 
+     * @param param Parametar koji se koristi za proveru preduslova.
+     * @throws Exception Ako parametar nije instanca {@link Clan} ili je {@code null}.
+     */
     @Override
     protected void preduslovi(Object param) throws Exception {
         if(param == null || !(param instanceof Clan))
             throw new Exception("Sistem ne moze da ucita clana");
     }
 
+        /**
+     * Izvršava operaciju ažuriranja podataka o članu u bazi podataka.
+     * <p>
+     * Ova metoda koristi {@code broker} za obavljanje ažuriranja podataka o članu.
+     * Rezultat operacije (da li je ažuriranje uspešno ili ne) se čuva u statičkoj promenljivoj {@code update}.
+     * </p>
+     * 
+     * @param param Parametar koji se koristi u operaciji, mora biti instanca {@link Clan}.
+     * @param kljuc Ključ koji se koristi u operaciji. Ova operacija ne koristi ovaj ključ.
+     * @throws Exception Ako dođe do greške tokom izvršenja operacije.
+     */
     @Override
     protected void izvrsiOperaciju(Object param, String kljuc) throws Exception {
         System.out.println("Usao u izvrsi oeraciju u AzurirajClanaOperacija");
--- Server/src/main/java/operacija/clan/UcitajClanoveOperacija.java
+++ Server/src/main/java/operacija/clan/UcitajClanoveOperacija.java
@@ -9,28 +9,73 @@ import java.util.List;
 import operacija.ApstraktnaGenerickaOperacija;
 
 /**
- *
+ * Klasa koja predstavlja operaciju učitavanja članova iz baze podataka.
+ * <p>
+ * Nasleđuje klasu {@link ApstraktnaGenerickaOperacija} i implementira konkretne metode
+ * za učitavanje podataka o članovima iz baze podataka. Ova klasa koristi šablon
+ * za obavljanje operacije i upravljanje transakcijama u bazi podataka.
+ * </p>
+ * 
  * @author Bogdan Blagojevic
  */
 public class UcitajClanoveOperacija extends ApstraktnaGenerickaOperacija {
 
+        /**
+     * Lista članova učitana iz baze podataka.
+     * <p>
+     * Ova promenljiva čuva rezultate operacije učitavanja članova iz baze podataka.
+     * </p>
+     */
     List<Clan> lista;
     
+    
+        /**
+     * Proverava preduslove za izvršenje operacije učitavanja članova.
+     * <p>
+     * Ova metoda osigurava da parametar koji se prosleđuje bude {@code null} ili instanca {@link Clan}.
+     * Ako parametar nije {@code null} i nije instanca {@link Clan}, baca se izuzetak sa odgovarajućom porukom.
+     * </p>
+     * 
+     * @param param Parametar koji se koristi za proveru preduslova.
+     * @throws Exception Ako parametar nije {@code null} i nije instanca {@link Clan}.
+     */
     @Override
     protected void preduslovi(Object param) throws Exception {
         if(param != null && !(param instanceof Clan))
             throw new Exception("Sistem ne moze da ucita clanove");
     }
 
+        /**
+     * Izvršava operaciju učitavanja članova iz baze podataka.
+     * <p>
+     * Ova metoda koristi {@code broker} za učitavanja članova iz baze podataka.
+     * Parametar {@code param} se ne koristi u ovoj operaciji. SQL upit za učitavanje članova uključuje 
+     * pridruživanje sa tabelom {@code mesto}.
+     * </p>
+     * 
+     * @param param Parametar koji se koristi u operaciji, u ovom slučaju se ne koristi.
+     * @param kljuc Ključ koji se koristi u operaciji, u ovom slučaju se ne koristi.
+     * @throws Exception Ako dođe do greške tokom izvršenja operacije.
+     */
     @Override
     protected void izvrsiOperaciju(Object param, String kljuc) throws Exception {
         lista = broker.getAll(new Clan(), " JOIN mesto mesto ON clan.mesto=mesto.mestoID");
     }
 
+        /**
+     * Get metoda - Vraća listu članova učitanu iz baze podataka.
+     * 
+     * @return Lista članova.
+     */
     public List<Clan> getLista() {
         return lista;
     }
 
+        /**
+     * Set metoda- Postavlja listu članova.
+     * 
+     * @param lista Lista članova koja će biti postavljena.
+     */
     public void setLista(List<Clan> lista) {
         this.lista = lista;
     }
