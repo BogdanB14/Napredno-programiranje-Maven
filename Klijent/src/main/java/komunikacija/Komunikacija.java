0df0aa3fc6bc856156749ee26fc928b45dfd7b63
Blagojevic-Bogdan <bb20190479@student.fon.bg.ac.rs>
2024-09-12 02:02:28 +0200

    Dokumentovane klase Kordinator i Komunikacija na klijentskoj strani

--- Klijent/src/main/java/komunikacija/Komunikacija.java
+++ Klijent/src/main/java/komunikacija/Komunikacija.java
@@ -22,7 +22,11 @@ import javax.swing.JOptionPane;
 import kordinator.Kordinator;
 
 /**
- *
+ * Klasa Komunikacija predstavlja komunikacioni sloj između klijentske aplikacije
+ * i servera. Koristi se za slanje zahteva i primanje odgovora sa servera za
+ * različite entitete kao što su članovi, treneri, grupe, administratori, itd.
+ * Implementira singleton šablon, što znači da postoji samo jedna instanca ove klase.
+ * 
  * @author Bogdan Blagojevic
  */
 public class Komunikacija {
@@ -31,16 +35,31 @@ public class Komunikacija {
     private Primalac primalac;
     private static Komunikacija instanca;
     
+    
+        /**
+     * Vraća jedinu instancu klase Komunikacija. Ukoliko instanca nije kreirana,
+     * kreira novu instancu.
+     * 
+     * @return jedinstvena instanca klase Komunikacija
+     */
     public static Komunikacija getInstanca(){
         if(instanca == null)
             instanca = new Komunikacija();
         return instanca;
     }
     
+        /**
+     * Privatni konstruktor klase kako bi se onemogućilo kreiranje više instanci.
+     */
     private Komunikacija(){
         
     }
     
+        /**
+     * Uspostavlja konekciju sa serverom na lokalnoj mašini koristeći port 9000.
+     * Kreira instance klasa Posiljalac i Primalac koje omogućavaju komunikaciju
+     * sa serverom.
+     */
     public void konekcija(){
         try {
             socket = new Socket("localhost",9000);
@@ -52,6 +71,14 @@ public class Komunikacija {
         }
     }
 
+        /**
+     * Omogućava prijavu administratora. Šalje zahteve za autentifikaciju serveru
+     * i prima odgovor o uspešnosti prijave.
+     * 
+     * @param korisnickoIme Korisničko ime administratora
+     * @param lozinka Lozinka administratora
+     * @return Administrator ukoliko je prijava uspešna, inače null
+     */
     public Administrator prijava(String korisnickoIme, String lozinka) {
         Administrator a = new Administrator();
         a.setKorisnickoIme(korisnickoIme);
@@ -64,6 +91,12 @@ public class Komunikacija {
         return administrator;
     }
 
+    
+    /**
+     * Učitava listu članova iz baze podataka putem zahteva poslatog serveru.
+     * 
+     * @return Lista članova
+     */
     public List<Clan> ucitajListuClanova() {
         Zahtev zahtev = new Zahtev(Operacija.UCITAJ_CLANOVE, null);
         List<Clan> lista;
@@ -75,6 +108,12 @@ public class Komunikacija {
         return lista;
     }
     
+    
+    /**
+     * Učitava listu mesta iz baze podataka putem zahteva poslatog serveru.
+     * 
+     * @return Lista mesta
+     */
     public List<Mesto> ucitajListuMesta() {
         Zahtev zahtev = new Zahtev(Operacija.UCITAJ_MESTA, null);
         List<Mesto> lista;
@@ -87,6 +126,12 @@ public class Komunikacija {
         
     }
 
+        /**
+     * Briše člana iz baze podataka na osnovu prosleđenog objekta Clan.
+     * 
+     * @param clan Objekat člana koji treba da se obriše
+     * @return Odgovor sa statusom operacije
+     */
     public Odgovor obrisiClana(Clan clan) {
         Zahtev zahtev = new Zahtev(Operacija.OBRISI_CLANA, clan);
         posiljalac.posalji(zahtev);
@@ -96,6 +141,12 @@ public class Komunikacija {
         
     }
 
+        /**
+     * Kreira novog člana u bazi podataka na osnovu prosleđenog objekta Clan.
+     * 
+     * @param clan Objekat člana koji treba da se kreira
+     * @return Odgovor sa statusom operacije
+     */
     public Odgovor kreirajClana(Clan clan) {
         Zahtev zahtev = new Zahtev(Operacija.DODAJ_CLANA, clan);
         System.out.println("ZAHTEEV U DODAJ CLANA U KOMUNIKACIJI: " + zahtev.getParametar());
@@ -106,6 +157,12 @@ public class Komunikacija {
         return odgovor;
     }
     
+        /**
+     * Kreira novog trenera u bazi podataka na osnovu prosleđenog objekta Trener.
+     * 
+     * @param trener Objekat trenera koji treba da se kreira
+     * @return Odgovor sa statusom operacije
+     */
     public Odgovor kreirajTrenera(Trener trener){
         Zahtev zahtev = new Zahtev(Operacija.DODAJ_TRENERA, trener);
         System.out.println("ZAHTEV U DODAJ TRENERA U KOMUNIKACIJI: " + zahtev.getParametar());
@@ -116,6 +173,12 @@ public class Komunikacija {
         return odgovor;
     }
     
+        /**
+     * Kreira novu grupu u bazi podataka na osnovu prosleđenog objekta Grupa.
+     * 
+     * @param grupa Objekat grupe koja treba da se kreira
+     * @return Odgovor sa statusom operacije
+     */
     public Odgovor kreirajGrupu(Grupa grupa) {
         Zahtev zahtev = new Zahtev(Operacija.DODAJ_GRUPU, grupa);
         System.out.println("ZAHTEV U DODAJ GRUPU U KOMUNIKACIJI: " + zahtev.getParametar());
@@ -126,6 +189,12 @@ public class Komunikacija {
         return odgovor;
     }
     
+    /**
+ * Kreira novi trening u bazi podataka na osnovu prosleđenog objekta Trening.
+ * 
+ * @param trening Objekat treninga koji treba da se kreira
+ * @return Odgovor sa statusom operacije
+ */
     public Odgovor kreirajTrening(Trening trening) {
         Zahtev zahtev = new Zahtev(Operacija.DODAJ_TRENING, trening);
         System.out.println("ZAHTEV U DODAJ TRENING U KOMUNIKACIJI: " + zahtev.getParametar());
@@ -136,6 +205,13 @@ public class Komunikacija {
         return odgovor;
     }
 
+    /**
+ * Ažurira podatke o članu u bazi podataka na osnovu prosleđenog objekta Clan.
+ * Nakon ažuriranja, osvežava glavnu formu.
+ * 
+ * @param clan Objekat člana koji treba da se ažurira
+ * @return Odgovor sa statusom operacije
+ */
     public Odgovor izmeniClana(Clan clan) {
         Zahtev zahtev = new Zahtev(Operacija.AZURIRAJ_CLANA, clan);
         System.out.println("ZAHTEEV U AZURIRAJ CLANA U KOMUNIKACIJI: " + zahtev.getParametar());
@@ -146,6 +222,12 @@ public class Komunikacija {
         return odgovor;
     }
 
+    
+/**
+ * Učitava listu treninga iz baze podataka.
+ * 
+ * @return Lista treninga
+ */
     public List<Trening> ucitajListuTreninga() {
         Zahtev zahtev = new Zahtev(Operacija.UCITAJ_TRENINGE, null);
         List<Trening> treninzi = new ArrayList<>();
@@ -158,6 +240,11 @@ public class Komunikacija {
         return treninzi;
      }
 
+    /**
+ * Učitava listu grupa iz baze podataka.
+ * 
+ * @return Lista grupa
+ */
     public List<Grupa> ucitajListuGrupa() {
         Zahtev zahtev = new Zahtev(Operacija.UCITAJ_GRUPE, null);
         List<Grupa> grupe = new ArrayList<>();
@@ -169,6 +256,11 @@ public class Komunikacija {
         
         return grupe;
     }
+    /**
+ * Učitava listu sala iz baze podataka.
+ * 
+ * @return Lista sala
+ */
     public List<Sala> ucitajListuSala() {
         Zahtev zahtev = new Zahtev(Operacija.UCITAJ_SALE,  null);
         List<Sala> sale = new ArrayList<>();
@@ -182,6 +274,11 @@ public class Komunikacija {
     }
     
 
+    /**
+ * Učitava listu kategorija iz baze podataka.
+ * 
+ * @return Lista kategorija
+ */
     public List<Kategorija> ucitajListuKategorija() {
         Zahtev zahtev = new Zahtev(Operacija.UCITAJ_KATEGORIJE, null);
         List<Kategorija> kategorije = new ArrayList<>();
@@ -194,6 +291,11 @@ public class Komunikacija {
         return kategorije;
     }
 
+    /**
+ * Učitava listu trenera iz baze podataka.
+ * 
+ * @return Lista trenera
+ */
     public List<Trener> ucitajListuTrenera() {
         Zahtev zahtev = new Zahtev(Operacija.UCITAJ_TRENERE, null);
         List<Trener> treneri = new ArrayList<>();
@@ -206,6 +308,11 @@ public class Komunikacija {
         return treneri;
     }
 
+    /**
+ * Učitava listu administratora iz baze podataka.
+ * 
+ * @return Lista administratora
+ */
     public List<Administrator> ucitajAdministratore() {
         Zahtev zahtev = new Zahtev(Operacija.UCITAJ_ADMINISTRATORE, null);
         List<Administrator> administratori = new ArrayList<>();
@@ -218,6 +325,9 @@ public class Komunikacija {
         return administratori;
     }
 
+    /**
+ * Odjavljuje trenutno prijavljenog administratora i zatvara socket konekciju.
+ */
     public void logOut() {
         try {
             Administrator a = Kordinator.getInstanca().getUlogovani();
--- Klijent/src/main/java/kordinator/Kordinator.java
+++ Klijent/src/main/java/kordinator/Kordinator.java
@@ -28,7 +28,15 @@ import kontroleri.PrikazGrupaController;
 import kontroleri.PrikazTreningaController;
 
 /**
- *
+ * Kordinator je singleton klasa koja koordinira otvaranjem formi i upravljanjem podacima
+ * unutar aplikacije. Ova klasa služi kao osnovni kontroler koji kontroliše formu za prijavu,
+ * glavnu formu i druge funkcionalnosti kao što su prikaz članova, grupa i treninga, 
+ * kao i dodavanje novih članova, trenera, grupa i treninga.
+ * 
+ * <p>Kordinator klasa upravlja interakcijom između korisničkog interfejsa (forme)
+ * i specijalizovanih kontrolera. Ova klasa takođe čuva informacije o trenutno ulogovanom administratoru
+ * i sadrži mehanizam za prosleđivanje parametara između različitih delova aplikacije.</p>
+ * 
  * @author Bogdan Blagojevic
  */
 public class Kordinator {
@@ -46,42 +54,74 @@ public class Kordinator {
     private Map<String, Object> parametri;
     
     
+        /**
+     * Vraća instancu klase Kordinator.
+     * Ukoliko instanca ne postoji, kreira se nova.
+     * 
+     * @return instanca klase Kordinator
+     */
     public static Kordinator getInstanca(){
         if(instanca == null)
             instanca = new Kordinator();
         return instanca;
     }
     
+        /**
+     * Privatni konstruktor koji kreira mapu parametara.
+     * Konstruktori su privatni kako bi se obezbedila primena Singleton šablona.
+     */
     private Kordinator(){
         parametri = new HashMap<>();
     }
 
+        /**
+     * Otvara formu za prijavu i kreira novi LoginController.
+     */
     public void otvoriLoginFormu() {
         loginController = new LoginController(new LoginForma());
         loginController.otvoriFormu();
         
     }
 
+        /**
+     * Otvara glavnu formu i kreira novi GlavnaFormaController.
+     */
     public void otvoriGlavnuFormu() {
         glavnaFormaController = new GlavnaFormaController(new GlavnaForma());
         glavnaFormaController.otvoriFormu();
         
     }
 
-
+    /**
+     * Otvara formu za prikaz članova i kreira novi PrikazClanaController.
+     */
     public void otvoriPrikazClanaFormu() {
         prikazClanaController = new PrikazClanaController(new PrikazClanaForma());
         prikazClanaController.otvoriFormu();
     }
     
+        /**
+     * Vraća trenutno ulogovanog administratora.
+     * 
+     * @return ulogovani administrator
+     */
     public Administrator getUlogovani() {
         return ulogovani;
     }
 
+        /**
+     * Postavlja trenutno ulogovanog administratora.
+     * 
+     * @param ulogovani administrator koji se postavlja
+     */
     public void setUlogovani(Administrator ulogovani) {
         this.ulogovani = ulogovani;
     }
 
+        /**
+     * Otvara formu za dodavanje novog člana i kreira novi DodajClanaController.
+     * Postavlja formu u mod za dodavanje.
+     */
     public void otvoriDodajClanaFormu() {
         dodajClanaController = new DodajClanaController(new DodajClanaForma());
         dodajClanaController.otvoriFormu(ModFormeClan.DODAJ);
@@ -90,6 +130,9 @@ public class Kordinator {
     }
 
 
+        /**
+     * Otvara formu za dodavanje novog trenera i kreira novi DodajTreneraController.
+     */
     public void otvoriDodajTreneraFormu() {
         System.out.println("USAO U OTVORI DODAJ TRENERA FORMU");
         dodajTreneraController = new DodajTreneraController(new DodajTreneraForma());
@@ -98,43 +141,75 @@ public class Kordinator {
     }
     
     
+        /**
+     * Osvežava tabelu članova pozivanjem metode za pripremu forme.
+     */
     public void osveziTabeluClanova() {
         prikazClanaController.pripremiFormu();
     }
     
+    
+    /**
+     * Dodaje parametar u mapu parametara.
+     * 
+     * @param s ključno ime parametra
+     * @param o vrednost parametra
+     */
     public void dodajParam(String s, Object o){
         parametri.put(s, o);
     }
     
+        /**
+     * Vraća parametar iz mape parametara na osnovu prosleđenog ključa.
+     * 
+     * @param s ključno ime parametra
+     * @return vrednost parametra
+     */
     public Object vratiParam(String s){
         return parametri.get(s);
     }
 
+       /**
+     * Otvara formu za izmenu člana i postavlja formu u mod za izmenu.
+     */
     public void otvoriIzmeniClanaFormu() {
         dodajClanaController = new DodajClanaController(new DodajClanaForma());
         dodajClanaController.otvoriFormu(ModFormeClan.IZMENI);
     }
 
+        /**
+     * Osvežava glavnu formu pozivanjem metode za pripremu forme za članove.
+     */
     public void osveziGlavnuFormu() {
         prikazClanaController.pripremiFormu();
     }
     
-    
+        /**
+     * Otvara formu za prikaz grupa i kreira novi PrikazGrupaController.
+     */
     public void otvoriPrikazGrupaFormu(){
         prikazGrupaController = new PrikazGrupaController(new PrikazGrupaForma());
         prikazGrupaController.otvoriFormu();
     }
     
+        /**
+     * Otvara formu za prikaz treninga i kreira novi PrikazTreningaController.
+     */
     public void otvoriPrikazTreningaFormu(){
         prikazTreningaController = new PrikazTreningaController(new PrikazTreningForma());
         prikazTreningaController.otvoriFormu();
     }
-
+    /**
+     * Otvara formu za dodavanje nove grupe i kreira novi DodajGrupuController.
+     */
     public void otvoriDodajGrupuForma() {
         dodajGrupuController = new DodajGrupuController(new DodajGrupuForma());
         dodajGrupuController.otvoriFormu();
     }
 
+        /**
+     * Otvara formu za dodavanje novog treninga i kreira novi DodajTreningController.
+     */
     public void otvoriDodajTreningFormu() {
         System.out.println("USAO U OTVORI DODAJ TRENING FORMU U KORDINATORU");
         dodajTreningController = new DodajTreningController(new DodajTreningForma());
